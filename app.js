efc8da3c25e9d2e051926da4897c7e28c3524b06
ZmijaZ <zmijaz2001@gmail.com>
2023-08-23 16:40:42 +0200

    add express ejs template with mongoose

---
+++ app.js
@@ -0,0 +1,54 @@
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+
+const mongoose = require("mongoose");
+
+var app = express();
+
+//mongoose
+mongoose.set("strictQuery", false);
+
+let url =
+  "mongodb+srv://zmijaz:zmijazmikac1@cluster0.ypttudu.mongodb.net/local_library?retryWrites=true&w=majority";
+
+mongoose
+  .connect(url)
+  .then(() => console.log("Connected to DB"))
+  .catch((err) => console.log("DB ERROR: " + err));
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+module.exports = app;
